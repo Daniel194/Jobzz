61bc2f975bbf193c94d500cef495ee3e4f522917
Daniel194 <lungu.daniel94@gmail.com>
2016-11-09 20:56:21 +0200

    First Commit.

---
+++ src/main/java/ro/jobzz/JobzzApplication.java
@@ -0,0 +1,12 @@
+package ro.jobzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobzzApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/jobzz/JobzzApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.jobzz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobzzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
