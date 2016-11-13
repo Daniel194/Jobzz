7ad411761a0b22c9ad4d0c43ae57c4529237abf1
Daniel194 <lungu.daniel94@gmail.com>
2016-11-13 19:18:30 +0200

    Refactoring.

---
+++ src/main/java/ro/jobzz/controller/Login.java
@@ -0,0 +1,27 @@
+package ro.jobzz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class Login {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+}
