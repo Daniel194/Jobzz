55cd1b714b2c04b61562a0ce782dde6bc2d00c1b
Daniel194 <lungu.daniel94@gmail.com>
2016-11-19 19:17:45 +0200

    Code refactoring.

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,16 +1,5 @@
-angular.module('jobzz', ['ngRoute'])
-    .config(function ($routeProvider, $httpProvider) {
-
-        $routeProvider.when('/', {
-            templateUrl: '/view/login.html',
-            controller: 'navigation',
-            controllerAs: 'controller'
-        }).otherwise('/');
-
-        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-
-    })
-    .controller('navigation', function ($rootScope, $http, $location) {
+angular.module('jobzz')
+    .controller('login', ['$rootScope','$http','$location',function ($rootScope, $http, $location) {
 
         var self = this;
 
@@ -67,4 +56,4 @@ angular.module('jobzz', ['ngRoute'])
                 $location.path("/");
             });
         }
-    });
+    }]);
