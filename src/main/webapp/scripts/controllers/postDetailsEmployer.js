ca10f2c86fd31ca5f113681dafd490bfc9933357
Daniel194 <lungu.daniel94@gmail.com>
2016-12-03 19:04:05 +0200

    Employer - Post Details - View - WIP.

--- src/main/webapp/scripts/controllers/postDetailsEmployer.js
+++ src/main/webapp/scripts/controllers/postDetailsEmployer.js
@@ -1,6 +1,6 @@
 angular.module('jobzz')
     .controller('PostDetailsEmployerCtrl', ['$scope', '$rootScope', '$http', 'postService', function ($scope, $rootScope, $http, postService) {
-
         $scope.post = postService.getPost();
+        $scope.latlng = [$scope.post.latitude , $scope.post.longitude];
 
     }]);
