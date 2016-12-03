f7540aec67f5803077cd4ffc099de7f6c0aa269d
Daniel194 <lungu.daniel94@gmail.com>
2016-12-03 21:41:10 +0200

    Employer - Post Details.

--- src/main/webapp/scripts/controllers/postDetailsEmployer.js
+++ src/main/webapp/scripts/controllers/postDetailsEmployer.js
@@ -1,6 +1,39 @@
 angular.module('jobzz')
     .controller('PostDetailsEmployerCtrl', ['$scope', '$rootScope', '$http', 'postService', function ($scope, $rootScope, $http, postService) {
         $scope.post = postService.getPost();
-        $scope.latlng = [$scope.post.latitude , $scope.post.longitude];
+        $scope.latlng = [$scope.post.latitude, $scope.post.longitude];
+
+        $scope.post.employeePostings = [{
+            price: 10,
+            currency: '$',
+            date: '2016-11-30',
+            comment: 'Ma intereseaza acest anunt',
+            employee: {
+                firstName: 'Matei',
+                lastName: 'Popescu',
+                reputation: 10
+            }
+
+        }, {
+            price: 13,
+            currency: '$',
+            date: '2016-11-29',
+            comment: 'Blabla blabla blabla',
+            employee: {
+                firstName: 'Tudor',
+                lastName: 'Parvu',
+                reputation: 12
+            }
+        }, {
+            price: 11,
+            currency: '$',
+            date: '2016-11-28',
+            comment: 'Test tes tes',
+            employee: {
+                firstName: 'Cristi',
+                lastName: 'Voicu',
+                reputation: 10
+            }
+        }]
 
     }]);
