291728c772975d2519a5d2f315b574c50348218a
Daniel194 <lungu.daniel94@gmail.com>
2016-12-30 14:16:59 +0200

    Fixed maven install problem.

--- src/main/java/ro/jobzz/Application.java
+++ src/main/java/ro/jobzz/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"application.properties", "database.properties"})
+@PropertySource(value = {"classpath:application.properties", "classpath:database.properties"})
 public class Application {
 
     public static void main(String[] args) {
