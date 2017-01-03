d1a1e36837c4544a297e8a9559b30ab5e432af95
Daniel194 <lungu.daniel94@gmail.com>
2017-01-03 21:02:35 +0200

    Employer - Settings - View

--- src/main/webapp/scripts/controllers/settingsEmployer.js
+++ src/main/webapp/scripts/controllers/settingsEmployer.js
@@ -1,5 +1,44 @@
 angular.module('jobzz')
     .controller('SettingsEmployerCtrl', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
 
+        $scope.currentDate = new Date();
+
+        $scope.minDate = new Date(
+            $scope.currentDate.getFullYear() - 100,
+            $scope.currentDate.getMonth(),
+            $scope.currentDate.getDate());
+
+        $scope.maxDate = new Date(
+            $scope.currentDate.getFullYear(),
+            $scope.currentDate.getMonth(),
+            $scope.currentDate.getDate());
+
+        $scope.minDateExp = new Date(
+            $scope.currentDate.getFullYear(),
+            $scope.currentDate.getMonth(),
+            $scope.currentDate.getDate());
+
+        $scope.maxDateExp = new Date(
+            $scope.currentDate.getFullYear() + 10,
+            $scope.currentDate.getMonth(),
+            $scope.currentDate.getDate());
+
+
+        $scope.changeGeneralInformation = function () {
+            //TODO
+        };
+
+
+        $scope.changePaymentInformation = function () {
+            //TODO
+        };
+
+        $scope.changePassword = function () {
+            //TODO
+        };
+
+        $scope.deleteAccount = function () {
+            //TODO
+        };
 
     }]);
