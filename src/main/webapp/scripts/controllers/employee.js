92100e19836ae3fddb240587e8bb4fe45805e4cc
Daniel194 <lungu.daniel94@gmail.com>
2017-01-12 21:36:31 +0200

    Employee - new Navbar.

--- src/main/webapp/scripts/controllers/employee.js
+++ src/main/webapp/scripts/controllers/employee.js
@@ -1,6 +1,6 @@
 angular.module('jobzz')
-    .controller('EmployeeCtrl', ['$scope', '$rootScope', '$http', 'EmployeeAuthSharedService', '$location',
-        function ($scope, $rootScope, $http, EmployeeAuthSharedService, $location) {
+    .controller('EmployeeCtrl', ['$scope', '$rootScope', '$http', 'EmployeeAuthSharedService', '$location', 'userProfilePictureService',
+        function ($scope, $rootScope, $http, EmployeeAuthSharedService, $location, userProfilePictureService) {
             $scope.currentNavItem = 'home';
 
             $scope.logoutEmployee = function () {
@@ -10,9 +10,29 @@ angular.module('jobzz')
             };
 
             $scope.navigateTo = function (newPath) {
-
                 $location.path(newPath).replace();
-
             };
+
+            $scope.openMenu = function ($mdOpenMenu, ev) {
+                $mdOpenMenu(ev);
+            };
+
+            (function () {
+                var req = {
+                    method: 'GET',
+                    dataType: 'json',
+                    url: '/employee/account',
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8'
+                    }
+                };
+
+                $http(req).then(function (response) {
+                    $scope.employee = response.data;
+                    $scope.employee.picture = userProfilePictureService.employeeProfilePicture($scope.employee.picture);
+                }, function () {
+                    //Empty
+                });
+            })();
 
         }]);
