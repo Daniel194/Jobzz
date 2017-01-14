fb13458627204f5990d3c52c3329223446118856
Daniel194 <lungu.daniel94@gmail.com>
2017-01-14 14:21:04 +0200

    Unit Test.

---
+++ src/test/java/ro/jobzz/RegisterControllerTest.java
@@ -0,0 +1,44 @@
+package ro.jobzz;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ro.jobzz.controllers.RegisterController;
+import ro.jobzz.services.EmployeeService;
+import ro.jobzz.services.EmployerService;
+import ro.jobzz.services.JobService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RegisterControllerTest {
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        JobService jobServices = Mockito.mock(JobService.class);
+        EmployerService employerService = Mockito.mock(EmployerService.class);
+        EmployeeService employeeService = Mockito.mock(EmployeeService.class);
+
+        this.mockMvc = standaloneSetup(new RegisterController(jobServices, employerService, employeeService)).build();
+    }
+
+    @Test
+    public void testGetJobs() throws Exception {
+        mockMvc.perform(get("/register/get/jobs").accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+}
